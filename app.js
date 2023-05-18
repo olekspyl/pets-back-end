cfa8862000f116931ff1080def72f72231165d58
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-05-18 11:40:25 +0200

    init commit

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const dotenv = require("dotenv");
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const contactsRouter = require('./routes/api/contacts')
+// const petsRouter = require("./routes/api/pets");
+// const authRouter = require("./routes/api/users");
 
-const app = express()
+const app = express();
+dotenv.config();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
+// app.use("/api/contacts", petsRouter);
+// app.use("/api/users", authRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  return res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json(message);
+});
 
-module.exports = app
+module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// const { DB_HOST } = process.env;
+const DB_HOST = 
+  "mongodb+srv://oleksa:jpd8gYrSerI48AMa@cluster0.oczcsol.mongodb.net/";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
