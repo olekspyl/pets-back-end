4a1893f8c2e5a490774c8de19a9634f5071411d6
Galina <galkozavrik@ukr.net>
2023-05-20 19:43:02 +0300

    small fix

--- controllers/notices/getNoticeByCategory.js
+++ controllers/notices/getNoticeByCategory.js
@@ -2,15 +2,12 @@ const { Notice } = require("../../models/notice");
 const { HttpError } = require("../../utils");
 
 const getNoticeByCategory = async (req, res) => {
-  console.log("it is getNoticeByCategory");
   const { type: category } = req.params;
   const { page = 1, limit = 10, search: title } = req.query;
   const skip = (page - 1) * limit;
 
-  console.log("it is getNoticeByCategory controller");
-  console.log(category);
   const query = title ? { category, title } : { category };
-  console.log("query", query);
+
   const result = await Notice.find(query, "-updatedAt -createdAt", {
     skip,
     limit,
