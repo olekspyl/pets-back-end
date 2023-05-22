568cde804494109e20c757730e02d07abf8f37b2
Mykola Saliuk <haker0502@ukr.net>
2023-05-22 16:37:32 +0300

    fix code
    - виносить пагінацію в окремий модуль
    
    - фіксить модель Notice

--- controllers/notices/getAllOwnerNotices.js
+++ controllers/notices/getAllOwnerNotices.js
@@ -1,16 +1,20 @@
 const { Notice } = require("../../models/notice");
+const { paginate } = require("../../utils");
 
 const getAllOwnerNotices = async (req, res) => {
   const { id: owner } = req.userId;
+  const { page: processedPage, limit: processedLimit } = req.query;
 
-  let { page = 1, limit = 12 } = req.query;
+  const { page, limit, skip } = paginate(processedPage, processedLimit);
 
-  const parsedPage = parseInt(page);
-  const parsedLimit = parseInt(limit);
-  page = parsedPage >= 1 ? parsedPage : 1;
-  limit = parsedLimit > 1 && parsedLimit < 12 ? parsedLimit : 12;
+  // let { page = 1, limit = 12 } = req.query;
 
-  const skip = (parseInt(page) - 1) * limit;
+  // const parsedPage = parseInt(page);
+  // const parsedLimit = parseInt(limit);
+  // page = parsedPage >= 1 ? parsedPage : 1;
+  // limit = parsedLimit > 1 && parsedLimit < 12 ? parsedLimit : 12;
+
+  // const skip = (parseInt(page) - 1) * limit;
 
   const totalNotices = await Notice.find({
     owner,
