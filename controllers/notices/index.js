aadd5396db9964fd66ba8d541cad653281a0967b
Galina <galkozavrik@ukr.net>
2023-05-23 14:26:41 +0300

    fix endpoints

--- controllers/notices/index.js
+++ controllers/notices/index.js
@@ -1,5 +1,5 @@
 const addNoticeInCategory = require("./addNoticeInCategory");
-const getNoticeByCategory = require("./getNoticeByCategory");
+// const getNoticeByCategory = require("./getNoticeByCategory");
 const remoweNoticeById = require("./remoweNoticeById");
 const getNoticeById = require("./getNoticeById");
 const getNoticeByName = require("./getNoticeByName");
@@ -12,7 +12,7 @@ const { ctrlWrapper } = require("../../utils");
 
 module.exports = {
   addNoticeInCategory: ctrlWrapper(addNoticeInCategory),
-  getNoticeByCategory: ctrlWrapper(getNoticeByCategory),
+  // getNoticeByCategory: ctrlWrapper(getNoticeByCategory),
   remoweNoticeById: ctrlWrapper(remoweNoticeById),
   getNoticeById: ctrlWrapper(getNoticeById),
   getNoticeByName: ctrlWrapper(getNoticeByName),
