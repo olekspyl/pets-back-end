3a27151c3e1775ed711e79c9291fbf185a01f73c
Mykola Saliuk <haker0502@ukr.net>
2023-05-30 09:01:37 +0300

    Update updateUserData.js

--- controllers/auth/updateUserData.js
+++ controllers/auth/updateUserData.js
@@ -23,7 +23,12 @@ const updateUserData = async (req, res) => {
     throw HttpError(404, "Not found");
   }
 
-  res.json(result);
+  const updatedUserObject = result.toObject();
+  delete updatedUserObject.password;
+
+  res.json(updatedUserObject);
+
+  // res.json(result);
 };
 
 module.exports = updateUserData;
